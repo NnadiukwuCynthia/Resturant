e93a98d368342fd80aa4f6d92eb885cb2e39c8d5
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2024-02-02 15:50:23 +0100

    task: Modified FAQ

---
+++ src/components/FAQ/FaqHero.jsx
@@ -0,0 +1,12 @@
+
+const FaqHero = () => {
+  return (
+    <div className="faqHero">
+        <div className="faqHero__content">
+            <p className="faqHero__content__text">frequently asked <br /> questions</p>
+        </div>
+    </div>
+  )
+}
+
+export default FaqHero
--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,8 +1,8 @@
-// import React from 'react'
+import FaqHero from "../components/FAQ/FaqHero"
 
 const Faq = () => {
   return (
-    <div>FAQ</div>
+    <FaqHero/>
   )
 }
 
