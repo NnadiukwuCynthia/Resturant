ea1e1c034708b44086bfa0511f9b9c514106bd1e
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2024-02-02 16:35:23 +0100

    Task: Modified Faq

---
+++ src/components/FAQ/FaqContent.jsx
@@ -0,0 +1,22 @@
+
+const FaqContent = () => {
+  return (
+    <div className="faqContent">
+      <p className="faqContent__Question">What is your address?</p>
+      <p className="faqContent__Answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br /> Our main entrance is through the Bresca front door.</p>
+      <p className="faqContent__Question">What are the hours of your operation?</p>
+      <p className="faqContent__Answer">cuisine is open for dinner service only, Wednesday to Sunday. <br />
+          <span className="faqContent__Answer__span">Seating Times</span> <br />
+          Wed, Thurs, Fri, and Sun : 5:30pm & 8:15pm <br />
+          Sat : 4pm, 6:30pm, 8:45pm.</p>
+      <p className="faqContent__Question">Does Cuisine have a dress Code?</p>
+      <p className="faqContent__Answer">LWe do not have a required dress code, however, most guests do dress in business or smart casual. <br />We do ask that our guests refrain from strongly scented perfumes and cologne. </p>
+      <p className="faqContent__Question">Do you have a valet parking?</p>
+      <p className="faqContent__Answer">We do not have valet parking. Street parking is available along 14th Street as well as the side streets. <br />We highly encourage our guests to arrange transportation. </p>
+      <p className="faqContent__Question">What are your corkage policy?</p>
+      <p className="faqContent__Answer">Corkage is $100 per 750ml.</p>
+    </div>
+  )
+}
+
+export default FaqContent
--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,8 +1,14 @@
 import FaqHero from "../components/FAQ/FaqHero"
+import FaqContent from './../components/FAQ/FaqContent';
+import Footer from '../components/Footer';
 
 const Faq = () => {
   return (
-    <FaqHero/>
+    <div>
+      <FaqHero/>
+      <FaqContent/>
+      <Footer/>
+    </div>
   )
 }
 
